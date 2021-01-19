1bf4ae346f19c0845fcf6125134ce4d8d32436da
mc36 <mc36@freemail.hu>
2021-01-19 06:42:41 +0000

    updating runtime lib

--- 02-PE-labs/update-src.sh
+++ 02-PE-labs/update-src.sh
@@ -5,4 +5,5 @@ rm rtr.zip
 mv a/misc/p4lang/forwarder.py p4src/
 mv a/misc/p4lang/router.p4 p4src/
 mv a/misc/p4lang/include/* p4src/include/
+mv a/misc/p4lang/p4runtime_lib/* p4src/p4runtime_lib/
 rm -Rf a
