a0ba77fb249bf3f84de94fd3a6cb8967561571ec
mc36 <mc36@p4lang.mchome.nop.hu>
2022-04-21 13:12:21 +0200

    updating source of thruth

--- 02-PE-labs/update-jar.sh
+++ 02-PE-labs/update-jar.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
 wget src.freertr.org/src/rtr.jar
 mv rtr.jar 0000-topology-A/bin/
+git add .
+git commit -m "updating jar"
+git push
--- 02-PE-labs/update-src.sh
+++ 02-PE-labs/update-src.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 wget src.freertr.org/rtr.zip
 mkdir a
 unzip -d a/ rtr.zip
@@ -7,3 +8,6 @@ mv a/misc/p4lang/router.p4 p4src/
 mv a/misc/p4lang/include/* p4src/include/
 mv a/misc/p4lang/p4runtime_lib/* p4src/p4runtime_lib/
 rm -Rf a
+git add .
+git commit -m "updating p4 sources"
+git push
