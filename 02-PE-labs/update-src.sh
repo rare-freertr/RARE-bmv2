a04892ff40c2bbb83a7aae707f393b1aab3cd397
mc36 <mc36@p4lang.mchome.nop.hu>
2024-10-07 13:37:46 -0500

    updating p4 sources

--- 02-PE-labs/update-src.sh
+++ 02-PE-labs/update-src.sh
@@ -3,10 +3,10 @@ wget src.freertr.org/rtr.zip
 mkdir a
 unzip -d a/ rtr.zip
 rm rtr.zip
-mv a/misc/p4lang/forwarder.py p4src/
-mv a/misc/p4lang/router.p4 p4src/
-mv a/misc/p4lang/include/* p4src/include/
-mv a/misc/p4lang/p4runtime_lib/* p4src/p4runtime_lib/
+mv a/misc/p4bmv2/forwarder.py p4src/
+mv a/misc/p4bmv2/router.p4 p4src/
+mv a/misc/p4bmv2/include/* p4src/include/
+mv a/misc/p4bmv2/p4runtime_lib/* p4src/p4runtime_lib/
 rm -Rf a
 git add .
 git commit -m "updating p4 sources"
